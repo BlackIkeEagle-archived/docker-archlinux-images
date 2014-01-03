b745bbd5ca9992c7d625749cfe8c9216956d4496
BlackEagle <ike.devolder@gmail.com>
2014-01-03 09:10:36 +0100

    create-docker-baseimg :: better cleanup
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- create-docker-baseimg.sh
+++ create-docker-baseimg.sh
@@ -8,9 +8,17 @@ mkdir -p "$1"
 
 pacstrap -d "$1" bash bzip2 coreutils file filesystem findutils gawk gcc-libs gettext glibc grep gzip inetutils iputils iproute2 less pacman perl procps-ng psmisc sed shadow tar texinfo util-linux which
 
+# clear packages cache
 rm -f "$1/var/cache/pacman/pkg/"*
-rm -rf "$1/usr/share/locale/"*
-rm -rf "$1/usr/share/i18n/"*
+
+# cleanup locale and manpage stuff, not needed to run in container
+toClean=('usr/share/locale' 'usr/share/i18n' 'usr/share/man')
+noExtract=''
+for clean in ${toClean[@]}; do
+	rm -rf "$1/$clean"/*
+	noExtract="$noExtract $clean/*"
+done
+sed -e "s,^#NoExtract.*,NoExtract = $noExtract," -i "$1/etc/pacman.conf"
 
 tar -C "$1" -c . | docker import - ike/archbase
 

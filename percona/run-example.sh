208fe8057a0d1cc704ca937c1707906909c460c9
BlackEagle <ike.devolder@gmail.com>
2014-07-13 10:37:14 +0200

    percona :: run-example simplified
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- percona/run-example.sh
+++ percona/run-example.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 (( EUID != 0 )) && echo 'This script must be run as root.' && exit 1
 
-if [[ ! -d "/var/lib/ike/docker/percona/mysql" ]]; then
-	mkdir -p /var/lib/ike/docker/percona/mysql
-fi
-docker run -v /var/lib/ike/docker/percona/mysql:/var/lib/mysql -d blackikeeagle/percona
+docker run -d blackikeeagle/percona

8ab765aba230142fcce5dca63ebaa9cc14393d0a
BlackEagle <ike.devolder@gmail.com>
2014-09-27 22:36:36 +0200

    drop nginx hhvm, i have no desire for SM
    Signed-off-by: BlackEagle <ike.devolder@gmail.com>

--- nginx-hhvm/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(( EUID != 0 )) && echo 'This script must be run as root.' && exit 1
-
-docker build -t blackikeeagle/nginx-hhvm .
--- nginx-hhvm/run-example.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(( EUID != 0 )) && echo 'This script must be run as root.' && exit 1
-
-docker run -d blackikeeagle/nginx-hhvm
